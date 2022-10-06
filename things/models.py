4432a4aadb74a1b602f026572ceb57a543836ef3
Vishal <vishal.hall@kcl.ac.uk>
2022-10-06 11:13:12 +0100

    Fix wrong model name

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import Model
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Model(Model):
+class Thing(Model):
     name = models.CharField(
         max_length=30, 
         unique=True, 
